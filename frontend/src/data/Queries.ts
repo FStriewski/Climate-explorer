fca374aa5e351d058d1b0e2c663ca9ae536326f9
Friedrich Striewski <friedrich.striewski@bynder.com>
2019-07-08 22:18:26 +0200

    Fix query

--- frontend/src/data/Queries.ts
+++ frontend/src/data/Queries.ts
@@ -15,21 +15,29 @@ type RequestParamsMonth = {
   month: string;
 } & RequestParams;
 
-export const getYear = (props: RequestParamsYear) => {
+export const getYear = async (props: RequestParamsYear) => {
   const { indicator, isoCountry, year } = props;
 
   if (!indicator || !isoCountry || !year) {
     return;
   }
-  return axios.get(`${baseUrl}/temp/${indicator}/${isoCountry}/${year}/`);
+  const response = await axios.get(
+    `${baseUrl}/temp/${indicator}/${isoCountry}/${year}/`
+  );
+
+  return response.data;
 };
 
-export const getMonthTimeSeries = (props: RequestParamsMonth) => {
+export const getMonthTimeSeries = async (props: RequestParamsMonth) => {
   const { indicator, isoCountry, month } = props;
   if (!indicator || !isoCountry || !month) {
     return;
   }
-  return axios.get(`${baseUrl}/getMonthTimeSeries/${indicator}/${isoCountry}/${month}/`);
+  const response = await axios.get(
+    `${baseUrl}/getMonthTimeSeries/${indicator}/${isoCountry}/${month}/`
+  );
+
+  return response.data;
 };
 
 export const getTimeSeries = async (props: RequestParams) => {
@@ -37,9 +45,10 @@ export const getTimeSeries = async (props: RequestParams) => {
   if (!indicator || !isoCountry) {
     return;
   }
-  
-  const response =  await axios.get(`${baseUrl}/timeSeries/${indicator}/${isoCountry}/`);
-  
-  console.log(response.data)
-  return response.data
+
+  const response = await axios.get(
+    `${baseUrl}/timeSeries/${indicator}/${isoCountry}/`
+  );
+
+  return response.data;
 };
